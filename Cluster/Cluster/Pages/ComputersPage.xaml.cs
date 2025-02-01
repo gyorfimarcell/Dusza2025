58dd46653f877ff73f76dd1c7e9711944cf768e4
Györfi Marcell <gyorfimarcell@gmail.com>
2025-02-01 18:47:57 +0100

    Move computer list to a seperate page

---
+++ Cluster/Cluster/Pages/ComputersPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace Cluster;
+
+public partial class ComputersPage : Page
+{
+    public ComputersPage()
+    {
+        InitializeComponent();
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        List<Computer> computers = Computer.GetComputers(MainWindow.ClusterPath);
+        icComputers.ItemsSource = computers.OrderBy(x => x.Name).Select(x => new ComputerRow(x));
+    }
+}
+
+public class ComputerRow(Computer computer)
+{
+    public string Name => computer.Name;
+
+    public int ProcessorCapacity => computer.ProcessorCore;
+    public int ProcessorUsage => computer.processes.Where(x => x.Active).Sum(x => x.ProcessorUsage);
+
+    public int MemoryCapacity => computer.RamCapacity;
+    public int MemoryUsage => computer.processes.Where(x => x.Active).Sum(x => x.MemoryUsage);
+}
