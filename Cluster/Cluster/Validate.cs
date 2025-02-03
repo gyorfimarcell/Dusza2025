80a3eefb0013a39c2b9b9470c48f64433f87faf8
palocziimi <70635760+palocziimi@users.noreply.github.com>
2025-02-03 12:58:39 +0100

    Validate filenames (#22)
    * Create Validate.cs
    
    * Update AddNewProgramPage.xaml.cs

---
+++ Cluster/Cluster/Validate.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cluster
+{
+    public class Validate
+    {
+        public static bool ValidateComputer(Computer computer)
+        {
+            if (computer == null)
+                return false;
+            if (string.IsNullOrEmpty(computer.Name))
+                return false;
+            if (computer.ProcessorCore < 1)
+                return false;
+            if (computer.RamCapacity < 1)
+                return false;
+            if (computer.processes == null)
+                return false;
+            if (computer.processes.Count == 0)
+                return false;
+            return true;
+        }
+        public static bool ValidateProcess(Process process)
+        {
+            if (process == null)
+                return false;
+            if (string.IsNullOrEmpty(process.ProgramName))
+                return false;
+            if (string.IsNullOrEmpty(process.ProcessId))
+                return false;
+            if (process.StartTime == null)
+                return false;
+            if (process.ProcessorUsage < 0)
+                return false;
+            if (process.MemoryUsage < 0)
+                return false;
+            return true;
+        }
+        public static bool ValidateProgramType(ProgramType programType)
+        {
+            if (programType == null)
+                return false;
+            if (string.IsNullOrEmpty(programType.ProgramName))
+                return false;
+            if (programType.ActivePrograms < 0)
+                return false;
+            if (programType.CpuMilliCore < 0)
+                return false;
+            if (programType.Memory < 0)
+                return false;
+            return true;
+        }
+
+        public static bool ValidateFileName(string fileName)
+        {
+            fileName = fileName.Trim();
+            Path.GetInvalidFileNameChars().Select(x => x.ToString()).ToList()
+                .ForEach(x => fileName = fileName.Replace(x, ""));
+
+            new List<string> { "CON", "PRN", "AUX", "NUL", "COM1–COM9", "LPT1–LPT9" }
+            .ForEach(x =>
+            {
+                fileName = fileName == x ? "" : fileName;
+            });
+
+            if (string.IsNullOrEmpty(fileName))
+                return false;
+            if (fileName.EndsWith('.'))
+                return false;
+            if (fileName.Length < 1)
+                return false;
+            return true;
+        }
+
+    }
+}
