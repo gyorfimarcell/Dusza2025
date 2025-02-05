3d08a2b55d1494ad68dc878534662d4331b08394
Györfi Marcell <gyorfimarcell@gmail.com>
2025-02-05 13:33:09 +0100

    Computer page filter and sorting

--- Cluster/Cluster/Pages/ComputersPage.xaml.cs
+++ Cluster/Cluster/Pages/ComputersPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using Wpf.Ui.Controls;
 using Button = Wpf.Ui.Controls.Button;
+using MenuItem = Wpf.Ui.Controls.MenuItem;
 using MessageBox = Wpf.Ui.Controls.MessageBox;
 using MessageBoxResult = Wpf.Ui.Controls.MessageBoxResult;
 
@@ -25,11 +26,45 @@ public partial class ComputersPage : CustomPage
     }
 
     public List<Computer> Computers;
+    ComputersPageSort sort = ComputersPageSort.Name;
 
     private void LoadData()
     {
-        Computers = Computer.GetComputers(MainWindow.ClusterPath).OrderBy(x => x.Name).ToList();
-        icComputers.ItemsSource = Computers;
+        Computers = Computer.GetComputers(MainWindow.ClusterPath);
+        UpdateFiltering();
+    }
+
+    internal enum ComputersPageSort
+    {
+        Name,
+        ProcessorUsage,
+        ProcessorUsagePercent,
+        ProcessorCapacity,
+        MemoryUsage,
+        MemoryUsagePercent,
+        MemoryCapacity
+    }
+
+    private void UpdateFiltering() {
+        IEnumerable<Computer> filtered = [.. Computers];
+
+        filtered = filtered.Where(x => tbFilter.Text == "" || x.Name.Contains(tbFilter.Text, StringComparison.InvariantCultureIgnoreCase));
+
+        filtered = sort switch
+        {
+            ComputersPageSort.Name => filtered.OrderBy(x => x.Name),
+            ComputersPageSort.ProcessorUsage => filtered.OrderBy(x => x.ProcessorUsage),
+            ComputersPageSort.ProcessorUsagePercent => filtered.OrderBy(x => x.ProcessorUsage / (double)x.ProcessorCore),
+            ComputersPageSort.ProcessorCapacity => filtered.OrderBy(x => x.ProcessorCore),
+            ComputersPageSort.MemoryUsage => filtered.OrderBy(x => x.MemoryUsage),
+            ComputersPageSort.MemoryUsagePercent => filtered.OrderBy(x => x.MemoryUsage / (double)x.RamCapacity),
+            ComputersPageSort.MemoryCapacity => filtered.OrderBy(x => x.RamCapacity),
+            _ => throw new NotImplementedException(),
+        };
+
+        if (MenuItemSortOrder.IsChecked) filtered = filtered.Reverse();
+
+        icComputers.ItemsSource = filtered;
     }
 
     private void MenuItemNew_OnClick(object sender, RoutedEventArgs e)
@@ -92,5 +127,24 @@ public partial class ComputersPage : CustomPage
             _window.RootSnackbarService.Show("Export complete", $"File saved to '{sfd.FileName}'",
                 ControlAppearance.Success, new SymbolIcon(SymbolRegular.Checkmark24), TimeSpan.FromSeconds(3));
         }
+    }
+
+    private void MenuItemSort_Click(object sender, RoutedEventArgs e)
+    {
+        MenuItem menuItem = (MenuItem)sender;
+        sort = (ComputersPageSort)Enum.Parse(typeof(ComputersPageSort), (string)menuItem.Tag);
+        UpdateFiltering();
+    }
+
+    
+
+    private void tbFilter_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateFiltering();
+    }
+
+    private void MenuItemSortOrder_Click(object sender, RoutedEventArgs e)
+    {
+        UpdateFiltering();
     }
 }
