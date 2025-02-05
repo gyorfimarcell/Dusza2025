c830e24493e4f5dcb4785e04ba72e6c449c9b152
Györfi Marcell <gyorfimarcell@gmail.com>
2025-02-05 10:26:59 +0100

    Move program shutdown to programs page

--- Cluster/Cluster/Pages/ProgramsPage.xaml.cs
+++ Cluster/Cluster/Pages/ProgramsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Ui.Controls;
 using Button = Wpf.Ui.Controls.Button;
+using MessageBox = Wpf.Ui.Controls.MessageBox;
+using MessageBoxResult = Wpf.Ui.Controls.MessageBoxResult;
+using TextBlock = Wpf.Ui.Controls.TextBlock;
 
 namespace Cluster
 {
@@ -27,7 +31,8 @@ namespace Cluster
             LoadData();
         }
 
-        public void LoadData() {
+        public void LoadData()
+        {
             Programs = ProgramType.ReadClusterFile(MainWindow.ClusterPath);
             icPrograms.ItemsSource = Programs;
         }
@@ -51,6 +56,42 @@ namespace Cluster
             Button card = (Button)sender;
             ProgramType program = (ProgramType)card.DataContext;
             _window.RootNavigation.NavigateWithHierarchy(typeof(ModifyProgramPage), program);
+        }
+
+        private async void btnShutdown_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+
+            Button card = (Button)sender;
+            ProgramType program = (ProgramType)card.DataContext;
+            int processCount = Computer.GetComputers(MainWindow.ClusterPath)
+                .Sum(x => x.processes.Count(y => y.ProgramName == program.ProgramName));
+
+            MessageBox messageBox = new MessageBox()
+            {
+                Title = $"Shut down {program.ProgramName}?",
+                Content = $"This will shut down all {processCount} processes.",
+                PrimaryButtonText = "Shutdown",
+                PrimaryButtonAppearance = ControlAppearance.Danger,
+            };
+
+
+            MessageBoxResult result = await messageBox.ShowDialogAsync();
+            if (result == MessageBoxResult.Primary)
+            {
+                if (ProgramType.ShutdownProgram(program))
+                { 
+                    Log.WriteLog([program.ProgramName, $"{program.CpuMilliCore}", $"{program.Memory}"], LogType.ShutdownProgram);
+                    _window.RootSnackbarService.Show(
+                        "Success",
+                        $"Program '{program.ProgramName}' successfully shut down!",
+                        ControlAppearance.Success,
+                        new SymbolIcon { Symbol = SymbolRegular.Checkmark24 },
+                        TimeSpan.FromSeconds(3)
+                    );
+                    LoadData();
+                }
+            }
         }
     }
 }
--- Cluster/Cluster/ProgramType.cs
+++ Cluster/Cluster/ProgramType.cs
@@ -60,22 +60,25 @@ namespace Cluster
             return $"{program.ProgramName}\n{program.ActivePrograms}\n{program.CpuMilliCore}\n{program.Memory}\n";
         }
 
-        public static bool ShutdownProgram(string path, List<ProgramType> programs, string programName)
+        public static bool ShutdownProgram(ProgramType program)
         {
-            if(!programs.Select(x => x.ProgramName).Contains(programName)) { return false; }
-            List<ProgramType> newPrograms = programs.Where(x => x.ProgramName != programName).ToList();
+            List<ProgramType> programs = ReadClusterFile(MainWindow.ClusterPath);
+            int index = programs.FindIndex(x => x.ProgramName == program.ProgramName);
+            if(index == -1) { return false; }
+
+            programs.RemoveAt(index);
             string fileContent = "";
-            foreach (var program in newPrograms)
+            foreach (var p in programs)
             {
-                fileContent += ClusterFileLines(program);
+                fileContent += ClusterFileLines(p);
             }
-            File.WriteAllText(path + "/.klaszter", fileContent);
-            foreach (var directory in Directory.GetDirectories(path)) 
+            File.WriteAllText(MainWindow.ClusterPath + "/.klaszter", fileContent);
+            foreach (var directory in Directory.GetDirectories(MainWindow.ClusterPath)) 
             {
                 string[] directoryFiles = Directory.GetFiles(directory);
                 foreach (var file in directoryFiles)
                 {
-                    if (Path.GetFileName(file).Contains(programName))
+                    if (Path.GetFileName(file).Contains(program.ProgramName))
                     {
                         File.Delete(file);
                     }
